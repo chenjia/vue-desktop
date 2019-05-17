0c9094d7745c1c30e597b6330770f55c7e0e22a0
chenjia <251746034@qq.com>
2019-05-17 22:12:44 +0800

    aa

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ window.Config = {
   server: 'http://47.100.119.102/api',
   key:'dzg5czJyejd5NjFkN3g3MjNjc2QwamN3OXUxODFjb280YWE4djhvYmV1NW9lemUxMzFjdGFiYTZ0OXo5dXhkMWNrYzRvZHZ3NjcyMzZkdWU=',
   random:new Date().getTime(),
-  preload: 3000
+  preload: 2000
 }
