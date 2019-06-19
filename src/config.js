77fcea50828e62166a9be4999aae635f1c550bf0
chenjia <251746034@qq.com>
2019-06-19 17:42:14 +0800

    aa

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 window.Config = {
   // server: 'http://localhost:8201/api',
-  server: 'http://47.100.119.102/api',
+  server: 'http'+(location.href.indexOf('https')==0?'s':'')+'://47.100.119.102/api',
   key:'dzg5czJyejd5NjFkN3g3MjNjc2QwamN3OXUxODFjb280YWE4djhvYmV1NW9lemUxMzFjdGFiYTZ0OXo5dXhkMWNrYzRvZHZ3NjcyMzZkdWU=',
   random:new Date().getTime(),
   preload: 2000
