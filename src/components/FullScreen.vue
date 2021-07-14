f27a39fa341526c212e8cebf3f075cbab8751ab4
chenjia <251746034@qq.com>
2021-07-14 14:00:15 +0800

    update

---
+++ src/components/FullScreen.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="fullScreen" style="position:absolute;" :style="{width:width+'px', height:(width*screenHeight/screenWidth)+'px'}">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fullScreen',
+  components: {
+    
+  },
+  data() {
+    return {
+      width: 1920
+    }
+  },
+  methods: {
+    
+  },
+  watch:{
+    
+  },
+  mounted(){
+    const scaleX = this.screenWidth/this.width
+    const scaleY = this.screenHeight/(this.width*this.screenHeight/this.screenWidth)
+    const fullScreen = this.$refs.fullScreen
+    if (fullScreen.style) {
+      fullScreen.style.transform = `scale(${scaleX}, ${scaleY})`
+      fullScreen.style.transformOrigin = '0 0'
+    }
+  }
+}
+</script>
+<style type="text/css">
+html {
+  font-size: 625%;
+}
+</style>
